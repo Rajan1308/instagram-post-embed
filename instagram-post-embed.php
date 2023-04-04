5716cc0cca7a04fadacfe24f83827611868674e8
Rajan Gupta <rajangpt74@gmail.com>
2023-04-05 00:24:47 +0530

    Uploading update script

---
+++ instagram-post-embed.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Instagram Post embed
+ * Plugin URI:  https://example.com/wp-maker-block
+ * Description: Instagram Post Embed plugin is a useful tool that allows users to easily embed Instagram posts into their WordPress website.
+ * Version:     1.0
+ * Author:      Rajan Gupta
+ * Author URI:  https://example.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-ipe
+ * Domain Path: /languages
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+function instagram_post_embed_block_init() {
+  wp_register_script(
+    'instagram-block',
+    plugins_url( 'block.js', __FILE__ ),
+	array( 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-editor' ),
+    filemtime( plugin_dir_path( __FILE__ ) . 'block.js' )
+  );
+
+  register_block_type( 'my-plugin/instagram-block', array(
+    'editor_script' => 'instagram-block',
+  ) );
+}
+add_action( 'init', 'instagram_post_embed_block_init' );
+
+
